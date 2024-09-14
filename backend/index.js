d58a2557f399b8868c15819fd8731cfb676c8015
João Lucas Melo de Oliveira <joao.oliveira307@fatec.sp.gov.br>
2024-09-14 15:02:42 -0300

    Rotas usuarios
    Rotas de usuários completas

--- backend/index.js
+++ backend/index.js
@@ -2,40 +2,64 @@ const express = require('express');
 const { PrismaClient } = require('@prisma/client');
 const app = express();
 const prisma = new PrismaClient();
+const upload = require('./upload');
 
 app.use(express.json());
+//Códigos de status usados
+//200 Sucesso!
+//201 Sucesso no cadastro!
+//400 Erro de requisição!
+//404 Página não encontrada!
+//500 Erro no servidor!
+
+
+
 
 //////////////////////////Usuários///////////////////////////////
 app.get('/usuarios', async (req, res) => {
-    try{
+    try {
         const usuarios = await prisma.usuario.findMany();
-        res.json(usuarios);
-    }catch{
-        res.json({mensagem:"Falha ao buscar usuários!"})
+        res.status(200).json(usuarios);
+    } catch (error) { 
+        res.status(500).json({ mensagem: "Falha ao buscar usuários!" });
     }
 });
 
-app.get('/usuarios/:id', async (req, res) => {
-    try{
-        const usuario = await prisma.usuario.findUnique({
-            where: { id: parseInt(req.params.id) }});
-        res.json(usuario)
-    }catch{
-        res.json({mensagem: "Falha ao buscar usuário!"})
+app.get('/usuarios/:email&:senha', async (req, res) => {
+    try {
+        const usuario = await prisma.usuario.findFirst({
+            where: {
+                AND: [
+                    { email: req.query.email },
+                    { senha: req.query.senha }
+                ]
+            }
+        });
+        if (usuario) {
+            res.status(200).json(usuario);
+        } else {
+            res.status(404).json({ mensagem: "Usuário não encontrado!" });
+        }
+    } catch (error) {
+        res.status(500).json({ mensagem: "Falha ao buscar usuário!" });
     }
-})
+});
 
-app.post('/usuarios', async (req, res) => {
-    try{
+app.post('/usuarios', upload.single('foto'), async (req, res) => {
+    try {
         const { nome, email, senha } = req.body;
-        const user = await prisma.usuario.create({
-            data: { nome, email, senha },
+        if (!req.file) {
+            return res.status(400).json({ mensagem: "a Foto é obrigatória!" });
+        }
+        const foto = req.file.buffer;
+        await prisma.usuario.create({
+            data: { nome, email, senha, foto },
         });
-        res.json({mensagem: "Usuário cadastrado com sucesso!",
-                  usuario: user
+        res.status(201).json({
+            mensagem: "Usuário cadastrado com sucesso!",
         });
-    }catch{
-        res.json({mensagem: "Falha ao cadastrar o usuário!"})
+    } catch (error) {
+        res.status(500).json({ mensagem: "Falha ao cadastrar o usuário!" });
     }
 });
 
