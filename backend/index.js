31fc8411a4586d1ccc220ba21a0c93df28cbe7ce
João Lucas Melo de Oliveira <joao.oliveira307@fatec.sp.gov.br>
2024-09-14 13:40:34 -0300

    Estrutura do frontend, backend rotas do usuario
    Feito a estrutura do frontend e feito as tabelas do prisma e a conexão com o sql server, rotas do usuário prontas(get, post)

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(express.json());
+
+//////////////////////////Usuários///////////////////////////////
+app.get('/usuarios', async (req, res) => {
+    try{
+        const usuarios = await prisma.usuario.findMany();
+        res.json(usuarios);
+    }catch{
+        res.json({mensagem:"Falha ao buscar usuários!"})
+    }
+});
+
+app.post('/usuarios', async (req, res) => {
+    try{
+        const { nome, email, senha } = req.body;
+        const user = await prisma.usuario.create({
+            data: { nome, email, senha },
+        });
+        res.json({mensagem: "Usuário cadastrado com sucesso!",
+                  usuario: user
+        });
+    }catch{
+        res.json({mensagem: "Falha ao cadastrar o usuário!"})
+    }
+});
+
+
+
+
+app.listen(3000, () => {
+  console.log('Servidor rodando na porta 3000');
+});
